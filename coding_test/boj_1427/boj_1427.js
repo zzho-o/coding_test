dabc4130f268633efbb0359c9f28590daf8da8ab
zzho-o <rkdwjsgh0@naver.com>
2022-03-25 23:49:21 +0900

    'coding_test'

--- coding_test/boj_1427/boj_1427.js
+++ coding_test/boj_1427/boj_1427.js
@@ -1,3 +1,4 @@
+'use strict';
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map(item => item.trim());
