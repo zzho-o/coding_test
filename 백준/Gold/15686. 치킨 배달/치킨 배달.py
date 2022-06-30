f431a4a8f1a860c397d11dd91f8e0fdb3d207eb0
zzho-o <78499273+zzho-o@users.noreply.github.com>
2022-06-30 19:38:50 +0900

    [Gold V] Title: 치킨 배달, Time: 708 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,28 +1,27 @@
 import sys
 from itertools import combinations
-
 input = sys.stdin.readline
-
-n, m = map(int, input().split())
-city = list(list(map(int, input().split())) for _ in range(n))
-result = 999999
-house = []      # 집의 좌표
-chick = []      # 치킨집의 좌표
-
+n,m = map(int,input().split())
+city = [list(map(int,input().split())) for i in range(n)]
+chick = []
+house = []
+def dist(h,c):
+    return abs(h[0] - c[0]) + abs(h[1] - c[1])
 for i in range(n):
     for j in range(n):
-        if city[i][j] == 1:
-            house.append([i, j])
-        elif city[i][j] == 2:
-            chick.append([i, j])
-
-for chi in combinations(chick, m):  # m개의 치킨집 선택
-    temp = 0            # 도시의 치킨 거리
-    for h in house: 
-        chi_len = 999   # 각 집마다 치킨 거리
-        for j in range(m):
-            chi_len = min(chi_len, abs(h[0] - chi[j][0]) + abs(h[1] - chi[j][1]))
-        temp += chi_len
-    result = min(result, temp)
-
-print(result)
+        if city[i][j] == 1 :
+            house.append([i,j])
+        elif city[i][j] == 2 :
+            chick.append([i,j])
+min_ = 9999
+for i in combinations(chick,m):
+    house_min = 0
+    for j in house:
+        min_chick = 9999
+        for k in i:
+            if min_chick > dist(j,k):
+                min_chick = dist(j,k)
+        house_min += min_chick
+    if min_ > house_min:
+        min_ = house_min
+print(min_)
